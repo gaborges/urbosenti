f4ea3596c5faab1e02ab2176bfe0301a6a48138f
Guilhermeborges.PF <Guilhermeborges.PF@81cd60a9-8bd7-644c-ffa7-456f7b5d24d1>
2014-08-11 18:23:45 +0000

    This folder will have the model of sensing module that will made in Java. This model will be used to construct other sensing modules.

---
+++ trunk/Sensing-Module-Model/src/urbosenti/core/communication/CommunicationManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urbosenti.core.communication;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class CommunicationManager {
+    
+}
---
+++ trunk/Sensing-Module-Model/src/urbosenti/core/data/DataManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urbosenti.core.data;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class DataManager {
+    
+}
