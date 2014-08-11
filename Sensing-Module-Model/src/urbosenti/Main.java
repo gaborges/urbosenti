e41d868786e38ce638c1cfafc657cde5d07f0050
Guilhermeborges.PF <Guilhermeborges.PF@81cd60a9-8bd7-644c-ffa7-456f7b5d24d1>
2014-08-11 18:23:45 +0000

    This folder will have the model of sensing module that will made in Java. This model will be used to construct other sensing modules.

---
+++ Sensing-Module-Model/src/urbosenti/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urbosenti;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Sensing-Module-Model/src/urbosenti/core/device/DeviceManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urbosenti.core.device;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class DeviceManager {
+    
+}
