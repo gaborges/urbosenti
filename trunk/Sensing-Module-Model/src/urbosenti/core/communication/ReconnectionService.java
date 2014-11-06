b622622ac8043d5a19f60472f29b02255e3060cb
Guilhermeborges.PF <Guilhermeborges.PF@81cd60a9-8bd7-644c-ffa7-456f7b5d24d1>
2014-11-06 18:36:53 +0000

    Códigos base de comunicação. OBS.: Necessita reimplementação dos métodos.
    Próximos passos: Implementar interfaces, serviço de entrega de mensagens, implementar monitoramento e definir ações possíveis.

--- trunk/Sensing-Module-Model/src/urbosenti/core/communication/ReconnectionService.java
+++ trunk/Sensing-Module-Model/src/urbosenti/core/communication/ReconnectionService.java
@@ -4,24 +4,100 @@
  */
 package urbosenti.core.communication;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Guilherme
  */
-public class ReconnectionService implements Runnable{
+public class ReconnectionService implements Runnable {
 
     private CommunicationManager communicationManager;
-    
-    public ReconnectionService(CommunicationManager cm) {
-        communicationManager = cm;
+    private List<CommunicationInterface> communicationInterfaces;
+    private boolean reconnected;
+    /**
+     * default 60 segundos Representado em milisegundos então (60 000)
+     */
+    private int reconnectionTime;
+    /**
+     * Se 1 = Tenta somente em um método. Default. Se 2 = Tenta em todos os
+     * métodos.
+     */
+    private int methodOfReconnection;
+
+    public ReconnectionService(CommunicationManager cm, List<CommunicationInterface> communicationInterfaces) {
+        this.communicationManager = cm;
+        this.communicationInterfaces = communicationInterfaces;
+        this.reconnectionTime = 60000;
+        this.methodOfReconnection = 1;
+        this.reconnected = false;
     }
-   
+
+    public synchronized void setReconnectionTime(int reconnectionTime) {
+        this.reconnectionTime = reconnectionTime;
+    }
+
+    public synchronized int getReconnectionTime() {
+        return reconnectionTime;
+    }
+
+    public void setReconnectionMethodOneAtTime() {
+        this.methodOfReconnection = 1;
+    }
+
+    public void setReconnectionMethodAllAtOnce() {
+        this.methodOfReconnection = 2;
+    }
+
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            reconectionProcess();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ReconnectionService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ReconnectionService.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void reconectionProcess(){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    public void reconectionProcess() throws InterruptedException, IOException {
+        CommunicationInterface current;
+
+        if (this.methodOfReconnection == 1) {
+            while (!reconnected) {
+                Iterator<CommunicationInterface> iterator = communicationInterfaces.iterator();
+                while (iterator.hasNext()) {
+                    current = iterator.next();
+                    synchronized (this) {
+                        wait(reconnectionTime);
+                    }
+                    if (current.testConnection()) {
+                        communicationManager.notifyReconnection(current);
+                        reconnected = true;
+                    }
+                }
+            }
+        }
+        if (this.methodOfReconnection == 2) {
+            while (!reconnected) {
+                synchronized (this) {
+                        wait(reconnectionTime);
+                    }
+                Iterator<CommunicationInterface> iterator = communicationInterfaces.iterator();
+                while (iterator.hasNext()) {
+                    current = iterator.next();
+                    if (current.testConnection()) {
+                        communicationManager.notifyReconnection(current);
+                        reconnected = true;
+                    } else {
+                        communicationManager.notifyReconnectionNotSucceed(current);
+                    }
+                }
+            }
+        }
     }
 }
---
+++ trunk/Sensing-Module-Model/src/urbosenti/core/communication/interfaces/WirelessCommunicationInterface.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urbosenti.core.communication.interfaces;
+
+import java.io.IOException;
+import urbosenti.core.communication.CommunicationInterface;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class WirelessCommunicationInterface extends CommunicationInterface{
+    
+    public WirelessCommunicationInterface() {
+        super();
+        setId(2);
+        setName("Wireless Interface");
+        setUsesMobileData(false);
+        setStatus(CommunicationInterface.STATUS_UNAVAILABLE);
+    }
+    
+    @Override
+    public boolean testConnection() throws IOException, UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean connect() throws IOException, UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean disconnect() throws IOException, UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public boolean isAvailable() throws IOException, UnsupportedOperationException {
+        setStatus(CommunicationInterface.STATUS_UNAVAILABLE);
+        return false;
+    }
+}
